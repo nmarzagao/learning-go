095b4aec28d731b690e62c3819d4b1be581ac590
Nicolas Marzagão <125494096+nmarzagao@users.noreply.github.com>
2023-11-06 20:13:30 +0000

    bubbletea is ok i guess

---
+++ tutorial/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+// Init 
+
+type model struct {
+	choices  []string // list of items
+	cursor   int
+	selected map[int]struct{} // item selected
+}
+
+func initialModel() model {
+	return model {
+		choices:  []string{"Buy carrots", "Buy celery", "Buy kohlrabi"},
+		selected: make(map[int]struct{}),
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+
+// Update
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+			
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor -= 1
+			}
+			
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor += 1
+			}
+
+		case "enter", " ":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+		}
+	}
+	return m, nil
+}
+
+// view
+
+func (m model) View() string {
+	s := "What should we buy at the market?\n\n"
+
+	for i, choice := range m.choices {
+		cursor := " "
+		if m.cursor == i {
+			cursor = ">"
+		}
+
+		checked := " "
+		if _, ok := m.selected[i]; ok {
+			checked = "x"
+		}
+
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
+
+	s += "\nPress q to quit. \n"
+	s += fmt.Sprintf("\n Debug { ItemsSelected: %v, Cursor: %v }", m.selected, m.cursor)
+
+	return s
+}
+
+func main() {
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, theres been an error: %v", err)
+		os.Exit(1)
+	}
+}
